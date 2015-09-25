8cd4338cf99a912fc01301b91c4163bc01ddf8cf
timkellogg <tim.kellogg@gmail.com>
2015-09-25 09:30:44 -0700

    add question model

---
+++ app/models/question.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr(),
+  content: DS.attr(),
+  notes: DS.attr(),
+  author: DS.attr(),
+  time: DS.attr()
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  return Ember.RSVP.hash({
+    questions: this.store.findAll('question')
+    // add categories later 
+  });
 });
