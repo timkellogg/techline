6c83b94510210f28efd708478111610e5d520a44
timkellogg <tim.kellogg@gmail.com>
2015-09-25 22:06:32 -0700

    add flash messages on questions/answers

--- app/routes/question.js
+++ app/routes/question.js
@@ -4,18 +4,32 @@ export default Ember.Route.extend({
 
   actions: {
     saveAnswer(params) {
+      var _this_ = this;
       var newAnswer = this.store.createRecord('answer', params);
       newAnswer.save();
-      params.question.save();
+      params.question.save()
+        .then(function() {
+          Ember.get(_this_, 'flashMessages').success('Answer was successfully created');
+        })
+        .catch(function() {
+          Ember.get(_this_, 'flashMessages').danger('Answer was not created');
+        });
       this.transitionTo('question');
     },
     updateQuestion(model, params) {
+      var _this_ = this;
       Object.keys(params).forEach(function(key) {
         if(params[key] !== undefined) {
           model.set(key, params[key]);
         }
       });
-      model.save();
+      model.save()
+        .then(function() {
+          Ember.get(_this_, 'flashMessages').success('Answer was successfully edited');
+        })
+        .catch(function() {
+          Ember.get(_this_, 'flashMessages').danger('Edit was unsuccessful');
+        });
       this.transitionTo('question', model.id);
     },
     deleteQuestion(model) {
@@ -26,12 +40,20 @@ export default Ember.Route.extend({
         Ember.RSVP.all(answer_deletions).then(function() {
           return model.destroyRecord();
         });
+        Ember.get(this, 'flashMessages').success('Answer was successfully deleted');
         this.transitionTo('index');
       }
     },
     deleteAnswer(answer) {
+      var _this_ = this;
       if(confirm('Are you sure you want to delete this answer?')) {
-        answer.destroyRecord();
+        answer.destroyRecord()
+          .then(function() {
+            Ember.get(_this_, 'flashMessages').success('Answer was successfully deleted');
+          })
+          .catch(function() {
+            Ember.get(_this_, 'flashMessages').danger('Edit was unsuccessful');
+          });
       }
     }
   }
