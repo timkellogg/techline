db158a995c8985c72f1abda85dbbca347d4cee57
timkellogg <tim.kellogg@gmail.com>
2015-09-27 23:12:38 -0700

    add fa dependency and css 2d transforms

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-
+    emberCliFontAwesome: {
+      useScss: true
+    }
   });
 
   app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
